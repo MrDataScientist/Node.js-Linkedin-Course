3f5dba15b9204afd42bb006a5123b9f744e825e5
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-10-11 06:19:14 -0700

    Create copyContent.js

---
+++ copyContent.js
@@ -0,0 +1,24 @@
+// copy the content of a file into another one
+
+let fs = require('fs');
+
+function readFile(file){
+  return fs.readFileSync(file, 'utf8');
+}
+
+function writeFile(file, content){
+  return fs.writeFile(file, content, handleError)
+}
+
+function handleError(err){
+  if(err){
+    return console.log('ahhhh!');
+  }
+  return console.log('the file was saved!');
+}
+
+function copyFile(target, src){
+  writeFile(target, readFile(src));
+}
+
+copyFile('./data.txt', './start.txt');
